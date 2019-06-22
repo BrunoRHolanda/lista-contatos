785278643aaca0f8f020ccb7b6783e786866e8f1
Bruno Rodrigues Holanda <brunorodriguesholanda@gmail.com>
2019-06-21 20:03:53 -0400

    corrigido arquivo de rotas com imports inválidos

--- resources/assets/vue/src/router.js
+++ resources/assets/vue/src/router.js
@@ -2,12 +2,6 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Home from '@vue/views/Home';
-import Sobre from '@vue/views/Sobre';
-import Produto from "@vue/views/catalogo/Produto";
-import Catalogo from "@vue/views/catalogo/Catalogo";
-import Loja from "@vue/views/Loja";
-import Contato from "@vue/views/Contato";
-import ProdutosCatalogo from "@vue/views/catalogo/ProdutosCatalogo";
 
 Vue.use(VueRouter);
 
