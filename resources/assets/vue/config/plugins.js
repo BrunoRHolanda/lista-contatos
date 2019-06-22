c779fc023380e1ce3303c7ea8062dbfd1027e2ab
Bruno Rodrigues Holanda <brunorodriguesholanda@gmail.com>
2019-06-22 18:10:31 -0400

    adicionado import de vuetify

--- resources/assets/vue/config/plugins.js
+++ resources/assets/vue/config/plugins.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
+import Vuetify from 'vuetify'
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(Vuetify);
