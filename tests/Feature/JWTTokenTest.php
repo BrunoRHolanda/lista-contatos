ae12a723099c2356a95c449170956c8c5a058fd7
Bruno Rodrigues Holanda <brunorodriguesholanda@gmail.com>
2019-06-21 23:52:58 -0400

    adicionado teste de token

---
+++ tests/Feature/JWTTokenTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class JWTTokenTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        // cria um usuário
+        $user = factory(User::class)->create([
+            'password' => bcrypt('123456'),
+        ]);
+
+        // tenta fazer o login
+        $response = $this->post('/api/auth/login', [
+            'email' => $user->email,
+            'password' => $user->password
+        ]);
+
+        $response->assertJsonStructure([
+            'access_token',
+            'token_type',
+            'expires_in'
+        ]);
+    }
+}
