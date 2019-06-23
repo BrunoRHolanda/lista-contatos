ffac9d29808434b24499105a784eb46809b7ae24
Bruno Rodrigues Holanda <brunorodriguesholanda@gmail.com>
2019-06-23 06:57:02 -0400

    criado funções para crud

--- resources/assets/vue/src/services/ContactsService.js
+++ resources/assets/vue/src/services/ContactsService.js
@@ -7,6 +7,21 @@ class ContactsService {
         Vue.http.get(API.resources.contacts)
             .then((response) => callback(response, null), (error) => callback(null, error));
     }
+
+    static store(contact, callback) {
+        Vue.http.post(API.resources.contacts, contact)
+            .then((response) => callback(response, null), (error) => callback(null, error));
+    }
+
+    static update(contact, callback) {
+        Vue.http.put(`${API.resources.contacts}/${contact.id}`, contact)
+            .then((response) => callback(response, null), (error) => callback(null, error));
+    }
+
+    static delete(contact, callback) {
+        Vue.http.delete(`${API.resources.contacts}/${contact.id}`)
+            .then((response) => callback(response, null), (error) => callback(null, error));
+    }
 }
 
 export default ContactsService;
