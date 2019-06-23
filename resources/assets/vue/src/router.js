7883dcffa852e4808c1e5db7b5369d68011d16f1
Bruno Rodrigues Holanda <brunorodriguesholanda@gmail.com>
2019-06-23 11:27:40 -0400

    refatorado router

--- resources/assets/vue/src/router.js
+++ resources/assets/vue/src/router.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router';
 
 import Contatos from '@vue/views/Contatos';
 import Login from "@vue/views/Login";
+import Home from "@vue/views/Home";
+import Perfil from "@vue/views/Perfil";
+import Menu from "@vue/components/Home/Menu";
 
 Vue.router = new VueRouter({
     mode: 'history',
@@ -12,10 +15,29 @@ Vue.router = new VueRouter({
             redirect: '/login'
         },
         {
-            path: '/contatos',
-            name: 'contatos',
-            component: Contatos,
+            path: '/app',
+            component: Home,
             meta: { auth: true },
+            children: [
+                {
+                    path: '/',
+                    name: 'app',
+                    component: Menu,
+                    meta: { auth: true },
+                },
+                {
+                    path: 'contatos',
+                    name: 'contatos',
+                    component: Contatos,
+                    meta: { auth: true },
+                },
+                {
+                    path: 'perfil',
+                    name: 'perfil',
+                    component: Perfil,
+                    meta: { auth: true },
+                },
+            ],
         },
         {
             path: '/login',
