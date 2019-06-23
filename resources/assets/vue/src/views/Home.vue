86b5dcf45624c89eeeaba8018113137de2834c8a
Bruno Rodrigues Holanda <brunorodriguesholanda@gmail.com>
2019-06-23 11:28:28 -0400

    criado uma view principal

---
+++ resources/assets/vue/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import Toolbar from "@vue/components/App/Toolbar";
+    export default {
+        name: "Home",
+        components: {Toolbar}
+    }
+</script>
+
+<style scoped>
+
+</style>
